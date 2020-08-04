aa16116f37b5289f28d4c7a048561ef1586621c6
Opatpong <patpong.t@muthvmail.com>
2020-08-04 19:07:06 +0700

    Lab2:input

--- src/lab2/Exercise_Lab2_1.java
+++ src/lab2/Exercise_Lab2_1.java
@@ -2,7 +2,22 @@ package lab2;
 
 import java.util.Scanner;
 
-public class Exercise_Lab2_1 {
- Scanner Scanner = new Scanner(System.in);
 
+    public class Exercise_Lab2_1 {
+        public static void main (String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String number;
+            System.out.print("Enter a number 1: ");
+            int a = scanner.nextInt();
+            System.out.print("Enter a number 2: ");
+            int b = scanner.nextInt();
+            System.out.print("Enter a number 3: ");
+            int c = scanner.nextInt();
+            System.out.print("Enter a number 4: ");
+            int d = scanner.nextInt();
+            System.out.print("Enter a number 5: ");
+            int f = scanner.nextInt();
+            //finding
+            int total = a + b + c + d + f;
+            int
 }
